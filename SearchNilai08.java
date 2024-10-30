7ef6541bcb32a6091fe807bc5a9bf72059b21d82
ddickyy <dikiidarmawam05x2@gmail.com>
2024-10-30 10:18:35 +0700

    Setelah soal

--- SearchNilai08.java
+++ SearchNilai08.java
@@ -1,19 +1,33 @@
+import java.util.Scanner;
+
 public class SearchNilai08 {
 
     public static void main(String[] args) {
-        int [] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner (System.in);
+
         int hasil = 0;
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int banyak = sc.nextInt();
+        int [] arrNilai = new int [banyak];
+
+        for (int i = 0; i < banyak ; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) +": ");
+            arrNilai [i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
 
         for (int i = 0; i < arrNilai.length; i++){
             if (key == arrNilai[i]) {
                 hasil = i;
+                System.out.println("Nilai " + key + " ketemu di index ke-" + hasil);
                 break;
-            }
+            } 
         }
-        System.out.println(); 
-        System.out.println("Nilai " + key + " ketemu di index ke-" + hasil);
-        System.out.println(); 
+        if (hasil == 0) {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
 
     }   
 }
