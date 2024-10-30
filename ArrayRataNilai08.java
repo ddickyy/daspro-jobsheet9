26a7b498bdeb1eea3d2a9c7084938a7c02f3e8f6
ddickyy <dikiidarmawam05x2@gmail.com>
2024-10-30 09:43:41 +0700

    Setelah soal

--- ArrayRataNilai08.java
+++ ArrayRataNilai08.java
@@ -3,19 +3,33 @@ import java.util.Scanner;
 public class ArrayRataNilai08 {
 
     public static void main(String[] args) {
-        Scanner sc =new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int [] nilaiMhs = new int [10];
-        double total = 0, rata2;
+        double totalLulus = 0;
+        double totalTidakLulus = 0;
+        int jumlahLulus = 0;
+        int jumlahTidakLulus = 0;
 
-        for (int i = 0; i < nilaiMhs.length; i++){
-            System.out.print("Masukkan nilai mahasiswa ke-" +(i+1) + " : ");
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
+        int [] nilaiMhs = new int[jmlMhs];
+
+        for (int i = 0; i < jmlMhs; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
+
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
+            }
         }
-        for (int i =0; i < nilaiMhs.length ; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " +rata2);
+
+        double rataLulus = (jumlahLulus > 0) ? totalLulus / jumlahLulus : 0;
+        double rataTidakLulus = (jumlahTidakLulus > 0) ? totalTidakLulus / jumlahTidakLulus : 0;
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
     }
 }
